274fdb6c696f9063f68906635f288b3be13f95a8
Sean Pierce <sumler.sean@gmail.com>
2023-03-31 22:08:18 -0700

    Add bandcamp embed

--- releases/hrg001/hrg001.php
+++ releases/hrg001/hrg001.php
@@ -3,21 +3,12 @@
     <h2>Sean Pierce - Sackerl</h2>
     <h3>Release date: April 10, 2023</h3>
 
-    <p>Sean Pierce brings us Sackerl for HRG001, which was composed and recorded in his shed, early Spring, 2023. This set was originally performed at Musique Plastique in Portland, Oregon, as part of the first Lonely God festival and reproduced for another performance at Mississippi Records the following week.</p>
+    <p><a href="https://seanpierce.net" target="_blank">Sean< Pierce</a> brings us Sackerl for HRG001, which was composed and recorded in his shed, early Spring, 2023. This set was originally performed at Musique Plastique in Portland, Oregon, as part of the first Lonely God festival and reproduced for another performance at Mississippi Records the following week.</p>
     <p>Cover artwork by Arthur Robbins (age 3). Layout and design by Hole Recording Gruppe. Mastered at The S'hole, Portland, Oregon.</p>
-
-    <a href="https://seanpierce.net" target="_blank">seanpierce.net</a>
 
     <p>Pre-order the digital and cassette releases on <a href="https://hole-recording-gruppe.bandcamp.com/album/sean-pierce-sackerl" target="_blank">bandcamp</a>.</p>
 
-    <strong>Track List</strong><br>
-    <ul>
-        <li>A1. EZ Up</li>
-        <li>A2. New Bag</li>
-        <li>B1. Creeps</li>
-        <li>B2. Head Will Lie</li>
-        <li>B3. Afternoon On A Hill</li>
-    </ul>
+    <iframe style="border: 0; width: 700px; height: 307px;" src="https://bandcamp.com/EmbeddedPlayer/album=3262620965/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="https://hole-recording-gruppe.bandcamp.com/album/sean-pierce-sackerl">Sean Pierce - Sackerl</a></iframe>
 
     <div class="flex-wrap">
         <div class="img-wrapper-half">
