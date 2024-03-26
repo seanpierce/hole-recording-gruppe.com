1ba13dc355d149abd362551c1508b4020cf314bd
seanpierce <sumler.sean@gmail.com>
2024-03-26 10:39:49 -0700

    Meta and style updates

---
+++ hole_recording_gruppe/helpers.py
@@ -0,0 +1,51 @@
+from urllib import request
+from releases.models import Release
+
+
+def get_meta_desctiption(release: Release | None) -> str:
+    """
+    Helper method used to 'sanitize' the info content from a release into a string that is meta-tag friendly.
+    First, turn line breaks into spaces, then replace double quotes with single quotes.
+    Next, remove any instances of the '&nbps;' entity. Finally, only take the first 150 characters.
+    If no release is supplied, method will return generic content.
+    """
+
+    generic_meta = 'Hole Recording Gruppe is a record label operating out of Portland, Oregon, USA.'
+
+    try:
+        if release is None:
+            return generic_meta
+        else:
+            return release.info.replace('<br>', ' ').replace('"', '\'').replace('&nbsp;', '')[:150]
+    except:
+        return generic_meta
+    
+
+def get_meta_image(release: Release | None, host: str) -> str:
+    """
+    Helper method used to return a meta image based on the first image associated with a release.
+    If no release is supplied, method wull return a generic image.
+    """
+
+    generic_image_path = f'{host}assets/images/logo-with-tagger.png'
+
+    try:
+        if release is None:
+            return generic_image_path
+        else:
+            return f'{host}/{release.image_set.first.url}'
+    except:
+        return generic_image_path
+
+    
+
+def scaffold_page_content(release: Release | None, host: str) -> dict:
+    """
+    Helper method used to scaffold meta data used in the application's head template.
+    """
+
+    return {
+        'meta_description': get_meta_desctiption(release),
+        'meta_image': get_meta_image(release, host),
+        'meta_url': host if release is None else f'{host}releases/{release.catalog}'
+    }
--- hole_recording_gruppe/views.py
+++ hole_recording_gruppe/views.py
@@ -2,13 +2,14 @@
 from datetime import date
 from django.shortcuts import redirect, render 
 
+from hole_recording_gruppe.helpers import scaffold_page_content
 from releases.services import get_releases, get_release_by_catalog
 
  
 def index(request):
     context = {
+        **scaffold_page_content(None, request.build_absolute_uri('/')),
         'title': 'hole recording gruppe',
-        'meta_description': 'Hole Recording Gruppe is a record label operating out of Portland, Oregon',
         'today': date.today(),
         'releases': get_releases()
     }
@@ -21,14 +22,11 @@ def release(request, catalog):
 
     if not release:
         return redirect("/404")
-
+    
     context = {
-        'title': release,
+        **scaffold_page_content(release, request.build_absolute_uri('/')),
+        'title': f'{release} | hole recording gruppe',
         'release': release,
-        'meta_description': release.info
-            .replace('<br>', ' ') # turn line breaks into spaces
-            .replace('"', '\'') # turn double quotes into single quotes
-            .replace('&nbsp;', '') # remove the '&nbps;' entity
     }
 
     return render(request, 'release.html', context)
@@ -36,8 +34,9 @@ def release(request, catalog):
 
 def not_found(request):
     context = {
-        'title': 'hole recording gruppe | 404',
-        'meta_description': 'Hole Recording Gruppe is a record label operating out of Portland, Oregon',
+        **scaffold_page_content(None, request.build_absolute_uri('/')),
+        'title': 'hole recording gruppe',
+        'release': release,
     }
 
     return render(request, '404.html', context)
