4f0472ff836737db8b8124e860b6c94a85225045
seanpierce <sumler.sean@gmail.com>
2024-03-28 09:34:13 -0700

    Update model to allow for nullable preorder date

--- releases/models.py
+++ releases/models.py
@@ -12,7 +12,7 @@ class Release(models.Model):
 	bandcamp_embed = models.TextField(null=True, blank=True)
 	active = models.BooleanField(default=True)
 	release_date = models.DateField()
-	preorder_date = models.DateField(null=True)
+	preorder_date = models.DateField(null=True, blank=True)
 
 	class Meta:
 		ordering = ['-id',]
